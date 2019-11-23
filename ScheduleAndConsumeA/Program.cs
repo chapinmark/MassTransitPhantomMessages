892d955e053ab9278976afa24ef7326b260b517a
mark chapin <chapinmark@gmail.com>
2019-11-22 22:58:25 -0700

    Add project files.

---
+++ ScheduleAndConsumeA/Program.cs
@@ -0,0 +1,84 @@
+﻿using MassTransit;
+using MassTransit.Scheduling;
+using Quartz;
+using System;
+using System.Threading.Tasks;
+
+namespace ScheduleAndConsumeA
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var busControl = ConfigureBus();
+
+            await busControl.StartAsync();
+            var scheduleSendEndPoint = await busControl.GetSendEndpoint(new Uri("rabbitmq://localhost/quartz"));
+            var scheduledRecurringMessage = await scheduleSendEndPoint.ScheduleRecurringSend(
+                     new Uri("rabbitmq://localhost/AMessage"),
+                     new ASchedule("/15 * * * * ?"),
+                     new AMessageImp());
+
+            do
+            {
+                Console.WriteLine("Enter q1 to exit without cancel, q2 to exit with cancel");
+                Console.Write("> ");
+                string value = Console.ReadLine();
+
+                if ("q1".Equals(value, StringComparison.OrdinalIgnoreCase))
+                    break;
+
+                if ("q2".Equals(value, StringComparison.OrdinalIgnoreCase))
+                {
+                    await scheduleSendEndPoint.CancelScheduledRecurringSend(scheduledRecurringMessage);
+                    break;
+                }
+
+            }
+            while (true);
+
+            await busControl.StopAsync();
+        }
+
+        static IBusControl ConfigureBus()
+        {
+            return Bus.Factory.CreateUsingRabbitMq(cfg =>
+            {
+                var host = cfg.Host(new Uri("rabbitmq://localhost"), h =>
+                {
+                    h.Username("guest");
+                    h.Password("guest");
+                });
+                cfg.ReceiveEndpoint(host, "AMessage", ep =>
+                {
+                    ep.Consumer<AConsumer>();
+                    EndpointConvention.Map<AMessage>(ep.InputAddress);
+
+                });
+                cfg.UseInMemoryScheduler();
+            });
+        }
+    }
+    public class ASchedule : DefaultRecurringSchedule
+    {
+        public ASchedule(string cronExpression)
+        {
+            CronExpression = cronExpression;
+        }
+    }
+
+    public interface AMessage
+    {
+    }
+    public class AMessageImp : AMessage
+    {
+    }
+    public class AConsumer : IConsumer<AMessage>
+    {
+        public Task Consume(ConsumeContext<AMessage> context)
+        {
+            Console.WriteLine($"Handled AMessage: {DateTime.Now}");
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ ScheduleAndConsumeB/Program.cs
@@ -0,0 +1,84 @@
+﻿using MassTransit;
+using MassTransit.Scheduling;
+using Quartz;
+using System;
+using System.Threading.Tasks;
+
+namespace ScheduleAndConsumeB
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var busControl = ConfigureBus();
+
+            await busControl.StartAsync();
+            var scheduleSendEndPoint = await busControl.GetSendEndpoint(new Uri("rabbitmq://localhost/quartz"));
+            var scheduledRecurringMessage = await scheduleSendEndPoint.ScheduleRecurringSend(
+                     new Uri("rabbitmq://localhost/BMessage"),
+                     new BSchedule("/15 * * * * ?"),
+                     new BMessageImp());
+
+            do
+            {
+                Console.WriteLine("Enter q1 to exit without cancel, q2 to exit with cancel");
+                Console.Write("> ");
+                string value = Console.ReadLine();
+
+                if ("q1".Equals(value, StringComparison.OrdinalIgnoreCase))
+                    break;
+
+                if ("q2".Equals(value, StringComparison.OrdinalIgnoreCase))
+                {
+                    await scheduleSendEndPoint.CancelScheduledRecurringSend(scheduledRecurringMessage);
+                    break;
+                }
+
+            }
+            while (true);
+
+            await busControl.StopAsync();
+        }
+
+        static IBusControl ConfigureBus()
+        {
+            return Bus.Factory.CreateUsingRabbitMq(cfg =>
+            {
+                var host = cfg.Host(new Uri("rabbitmq://localhost"), h =>
+                {
+                    h.Username("guest");
+                    h.Password("guest");
+                });
+                cfg.ReceiveEndpoint(host, "BMessage", ep =>
+                {
+                    ep.Consumer<BConsumer>();
+                    EndpointConvention.Map<BMessage>(ep.InputAddress);
+
+                });
+                cfg.UseInMemoryScheduler();
+            });
+        }
+    }
+    public class BSchedule : DefaultRecurringSchedule
+    {
+        public BSchedule(string cronExpression)
+        {
+            CronExpression = cronExpression;
+        }
+    }
+
+    public interface BMessage
+    {
+    }
+    public class BMessageImp : BMessage
+    {
+    }
+    public class BConsumer : IConsumer<BMessage>
+    {
+        public Task Consume(ConsumeContext<BMessage> context)
+        {
+            Console.WriteLine($"Handled BMessage: {DateTime.Now}");
+            return Task.CompletedTask;
+        }
+    }
+}
